bfafd2ad0f7571a53bb1f1116465c8751b42c5a9
Kajeli-bit <kajeliemmanuel@gmail.com>
2026-08-24 16:38:38 +0300

    sessions

--- src/lib/auth.ts
+++ src/lib/auth.ts
@@ -33,6 +33,7 @@ export const { handlers, signIn, signOut, auth } = NextAuth({
   ],
   session: {
     strategy: "jwt",
+    maxAge: 30 * 24 * 60 * 60, // 30 days
   },
   pages: {
     signIn: "/admin/login",
